71ecf0663a2d6bef78a7cc1bb83985776fa5a7d2
Balaji KG <balaji.kg@ascendlearning.com>
2025-01-29 12:39:51 +0530

    Product Managment unit functionality implemented

---
+++ src/app/admin-dashboard/product-managment/product-managment.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-managment',
+  imports: [CommonModule],
+  templateUrl: './product-managment.component.html',
+  styleUrl: './product-managment.component.css'
+})
+export class ProductManagmentComponent {
+  products : Product[] = [];
+
+  constructor(private router:Router , private productService:ProductService){}
+
+  ngOnInit():void{
+    this.loadProducts();
+  }
+
+  loadProducts(){
+    this.productService.getAllProducts().subscribe(
+      data => {
+        this.products = data;
+      },error => {
+        console.log('Error Fetching Producta',error);
+      }
+    )
+  }
+
+  createNewProduct(){
+    this.router.navigate(['admin/add-product']);
+  }
+
+  editProduct(id:number):void{
+    this.router.navigate(['admin/add-product',id])
+  }
+
+  deleteProduct(id:number):void{
+    
+  }
+}
